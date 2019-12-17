3ed20f22ce11edc6260146ac8f84045e24e0e7d4
Brubatera <brubatera5@gmail.com>
2019-12-16 21:52:36 -0300

    projeto criado

---
+++ src/main/java/com/bruno/dev/spring/CursoUdemySpringApplication.java
@@ -0,0 +1,13 @@
+package com.bruno.dev.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoUdemySpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoUdemySpringApplication.class, args);
+	}
+
+}
