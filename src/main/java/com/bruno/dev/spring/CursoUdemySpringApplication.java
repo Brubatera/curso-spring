e424120f0cec1d8ea4c3a9ebd1cd340e0beb9f16
Brubatera <brubatera5@gmail.com>
2019-12-22 17:15:03 -0300

    Operacao de instanciacao;

--- src/main/java/com/bruno/dev/spring/CursoUdemySpringApplication.java
+++ src/main/java/com/bruno/dev/spring/CursoUdemySpringApplication.java
@@ -1,13 +1,34 @@
 package com.bruno.dev.spring;
 
+import com.bruno.dev.spring.domain.Category;
+import com.bruno.dev.spring.repositories.RepositoryCategory;
+import lombok.AllArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
+@AllArgsConstructor
 @SpringBootApplication
-public class CursoUdemySpringApplication {
+public class CursoUdemySpringApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CursoUdemySpringApplication.class, args);
-	}
+    private RepositoryCategory repository;
 
+    public static void main(String[] args) {
+        SpringApplication.run(CursoUdemySpringApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        Category cat1 = Category.builder()
+                .name("Informática")
+                .build();
+
+        Category cat2 = Category.builder()
+                .name("Escritório")
+                .build();
+
+        repository.saveAll(Arrays.asList(cat1, cat2));
+    }
 }
