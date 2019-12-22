ce80b0dab2b6d83c6863150f887b0b70097f25e2
Brubatera <brubatera5@gmail.com>
2019-12-22 13:11:44 -0300

    BD H2 e criacao automática da base de dados

--- src/main/java/com/bruno/dev/spring/domain/Category.java
+++ src/main/java/com/bruno/dev/spring/domain/Category.java
@@ -2,6 +2,10 @@ package com.bruno.dev.spring.domain;
 
 import lombok.*;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.io.Serializable;
 
 @AllArgsConstructor
@@ -10,8 +14,11 @@ import java.io.Serializable;
 @Setter
 @EqualsAndHashCode(of = "id")
 @Builder
+@Entity
 public class Category implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
     private String name;
 }
--- src/main/java/com/bruno/dev/spring/resources/CategoriesResource.java
+++ src/main/java/com/bruno/dev/spring/resources/CategoriesResource.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -26,8 +25,7 @@ public class CategoriesResource {
                 .name("Escritório")
                 .build();
 
-        List<Category> list = Arrays.asList(cat1, cat2);
 
-        return list;
+        return Arrays.asList(cat1, cat2);
     }
 }
